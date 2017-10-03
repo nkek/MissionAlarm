227f1abbd5c187fae9fc7740bfb1f1804f05cfce
eunkyung.namkung <skarnd88@gmail.com>
2017-10-03 13:49:06 +0900

    First commit

---
+++ app/src/main/java/com/appspot/nkek/missionalarm/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.appspot.nkek.missionalarm
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
---
+++ app/src/main/java/com/appspot/nkek/missionalarm/view/AlarmDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.appspot.nkek.missionalarm.view
+
+import android.app.Activity
+import android.os.Bundle
+import com.appspot.nkek.missionalarm.R
+
+class AlarmDetailActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_detail2)
+    }
+}
